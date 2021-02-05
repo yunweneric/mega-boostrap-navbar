1c71fc94e3e83f0fd32e93cb1207df63018ef7f4
yunwen <yunweneric@gmail.com>
2021-02-05 17:39:18 +0100

    complete navbar

---
+++ assets/scripts/script.js
@@ -0,0 +1,21 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = () => {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+    navbar.classList.add("container1")
+  } else {
+    navbar.classList.remove("sticky");
+    console.log(sticky);
+
+  }
+}
